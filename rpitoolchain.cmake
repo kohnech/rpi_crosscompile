baeb499ce0f6701a7c2b2dbfa1b30af22e3c5cf6
Christian Köhnenkamp <cvk5@me.com>
2021-11-28 22:31:58 +0100

    remove not used sysroot path

--- rpitoolchain.cmake
+++ rpitoolchain.cmake
@@ -6,4 +6,3 @@ set(CMAKE_SYSTEM_VERSION 1)
 
 set(CMAKE_C_COMPILER   /opt/cross-pi-gcc/bin/arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER /opt/cross-pi-gcc/bin/arm-linux-gnueabihf-g++)
-set(CMAKE_SYSROOT      /workspaces/rpi_crosscompile/tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/arm-bcm2708-linux-gnueabi/sysroot)
